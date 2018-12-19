ffc52e8be01196c17eb5b61750d147c12e787f96
Korengast <32592798+Korengast@users.noreply.github.com>
2018-12-19 13:55:55 +0200

    Compare average #views to the average #views of best 100

--- data_preparation_v2.py
+++ data_preparation_v2.py
@@ -2,6 +2,8 @@ __author__ = "Koren Gast"
 
 import pandas as pd
 import copy
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Files dir
 sample_segments1_path = "C:\\Users\\koren\\PycharmRep\\Taboola\\raw_data\\sample_segments1.csv"
@@ -9,17 +11,41 @@ sample_segments2_path = "C:\\Users\\koren\\PycharmRep\\Taboola\\raw_data\\sample
 
 s_seg_df = pd \
     .read_csv(sample_segments1_path) \
-    .append(pd
-            .read_csv(sample_segments2_path), ignore_index=True)
-
-s_tot_df = s_seg_df.groupby(['campaign_id', 'd'])\
-    .sum()\
-    .reset_index()\
-    .drop('user_data_directory_id', axis=1)\
-    .rename({"d_segment_views": "views", "d_segment_clicks": "clicks"}, axis = 'columns')
+    .append(pd.read_csv(sample_segments2_path), ignore_index=True) \
+    .dropna()
 
 s_seg_df['clicks_ratio'] = s_seg_df['d_segment_clicks'] / s_seg_df['d_segment_views']
-s_tot_df['clicks_ratio'] = s_tot_df['clicks'] / s_tot_df['views']
+
+# Plotting click ratios vs #views.
+plt.scatter(s_seg_df['d_segment_views'], s_seg_df['clicks_ratio'])
+plt.show()
+
+# Plotting click ratios vs #views only for #views > median
+zoom_df = s_seg_df[s_seg_df['d_segment_views'] >= \
+                   np.median(s_seg_df['d_segment_views'])]
+plt.scatter(zoom_df['d_segment_views'], zoom_df['clicks_ratio'])
+plt.show()
+
+# Correcting the clicks_ratio calculation
+s_seg_df['clicks_ratio'] = s_seg_df['d_segment_clicks'] / (s_seg_df['d_segment_views'])**0.5
+
+# Plot again
+plt.scatter(s_seg_df['d_segment_views'], s_seg_df['clicks_ratio'])
+plt.show()
+zoom_df = s_seg_df[s_seg_df['d_segment_views'] >= \
+                   np.median(s_seg_df['d_segment_views'])]
+plt.scatter(zoom_df['d_segment_views'], zoom_df['clicks_ratio'])
+plt.show()
+
+s_seg_df = s_seg_df[s_seg_df['d_segment_views'] >= \
+                    np.percentile(s_seg_df['d_segment_views'], 25)]
+
+# Calculate the equivalent to the "total" csv
+s_tot_df = s_seg_df[['campaign_id', 'd', 'd_segment_views', 'clicks_ratio']] \
+    .groupby(['campaign_id', 'd']) \
+    .sum() \
+    .reset_index()
+
 
 def calc_click_rate(click_ratio, campaign, date):
     # Calculating click rate of a campaign-segment by comparing
@@ -27,6 +53,7 @@ def calc_click_rate(click_ratio, campaign, date):
     # to the average click ratio for this date and campaign
     row = s_tot_df[s_tot_df['campaign_id'] == campaign][s_tot_df['d'] == date]
     return float(click_ratio / row['clicks_ratio'])
+
 
 df = copy.deepcopy(s_seg_df)
 s_seg_df['click_rate'] = df. \
--- find_best_couples.py
+++ find_best_couples.py
@@ -1,19 +1,19 @@
 __author__ = "Koren Gast"
 
-# import data_preparation
+# import data_preparation_v2
 import pandas as pd
 import numpy as np
 
 segments_path = "data_v2\\segments.csv"
 segments = pd.read_csv(segments_path)
-segments = segments.dropna()
 
-segments = segments[segments['d_segment_views'] >=
-                    np.median(segments['d_segment_views'])]
+sorted_df = segments[['campaign_id', 'user_data_directory_id', 'd_segment_views', 'click_rate']] \
+    .groupby(['campaign_id', 'user_data_directory_id']) \
+    .sum() \
+    .reset_index() \
+    .sort_values('click_rate', ascending=0)
 
-best_100 = segments[['campaign_id', 'user_data_directory_id', 'd_segment_views', 'click_rate']]\
-    .groupby(['campaign_id', 'user_data_directory_id'])\
-    .sum()\
-    .reset_index()\
-    .sort_values('click_rate', ascending=0)\
-    .head(100)
+print('Average views: {}'.format(np.mean(sorted_df['d_segment_views'])))
+print('Average best 100 views: {}'.format(np.mean(sorted_df.head(100)['d_segment_views'])))
+
+sorted_df.head(100).to_csv('best_100')
